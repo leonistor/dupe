11d049f1ad3ddaf9b36656ced94ac7ddd87cec66
Leo Nistor <leo.nistor@gmail.com>
2020-04-08 10:13:16 +0300

    Gatherer, Worker

--- lib/duper/application.ex
+++ lib/duper/application.ex
@@ -6,10 +6,12 @@ defmodule Duper.Application do
   def start(_type, _args) do
     children = [
       Duper.Results,
-      {Duper.PathFinder, "."}
+      {Duper.PathFinder, "."},
+      Duper.WorkerSupervisor,
+      {Duper.Gatherer, 1}
     ]
 
-    opts = [strategy: :one_for_one, name: Duper.Supervisor]
+    opts = [strategy: :one_for_all, name: Duper.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
